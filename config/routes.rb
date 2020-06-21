92246009f5237df3e1c2b330f63a5c90a58bd3e3
Viktor <macedonian.wow@live.com>
2020-06-21 22:47:40 +0200

    Creation of Tweet show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'tweets/index', to: 'tweets#index'
+  get "/articles/:id", to: "articles#show"
 
   root to: 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
