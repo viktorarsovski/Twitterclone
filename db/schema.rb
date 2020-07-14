0fd3260a8642aebd2471ffb6b6e5096b7c756d16
Viktor <macedonian.wow@live.com>
2020-07-14 21:48:51 +0200

    Fix db:migrate and form in comments

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_07_05_203056) do
 
   create_table "comments", force: :cascade do |t|
+    t.string "commenter"
     t.text "body"
     t.integer "tweet_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.index ["tweet_id"], name: "index_comments_on_tweet_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "tweets", force: :cascade do |t|
@@ -27,8 +26,7 @@ ActiveRecord::Schema.define(version: 2020_07_05_203056) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
+  add_foreign_key "comments", "tweets"
 end
