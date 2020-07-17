016c3e64a25d4ec314ff5334f01db7f724fe95bf
Viktor <macedonian.wow@live.com>
2020-07-18 01:28:22 +0200

    New User model with model validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,5 @@ class Tweet < ApplicationRecord
   has_many :comments
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { maximum: 140 }
 end
