a37dca57e578c025be2c806650030491d91ff884
Viktor <macedonian.wow@live.com>
2020-07-18 16:16:55 +0200

    Users Controller with :new, :create and :show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-
-  get 'users/new'
-  get 'users/create'
   root to: 'tweets#index'
 
   resources :tweets do
     resources :comments
   end
 
-  resources :users
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
