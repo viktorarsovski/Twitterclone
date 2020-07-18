0326566fc0dfad430027ae59c29dd77c72cb82e6
Viktor <macedonian.wow@live.com>
2020-07-18 19:27:15 +0200

    Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-
   root to: 'tweets#index'
 
-  resources :tweets
   resources :tweets do
     resources :comments
   end
+
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
