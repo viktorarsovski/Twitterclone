7ceb1f1b8ef795184cb1c3bae9b767068b04f908
Viktor <macedonian.wow@live.com>
2020-07-18 15:25:39 +0200

    Add password attribute to the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_232617) do
+ActiveRecord::Schema.define(version: 2020_07_18_132214) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_232617) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
